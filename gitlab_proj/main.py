ede252bacf899edb8ad0e0c64d7ce209bddf472b
iroshanav <knightshift.kurosaki@gmail.com>
2024-08-17 19:29:36 +0530

    Add | Models and Requirements

---
+++ gitlab_proj/main.py
@@ -0,0 +1,13 @@
+# author    https://github.com/iroshanvidanage
+# date      08/17/2024
+
+
+def main():
+    print(f'Application starting.\n')
+    print('''
+    Employee Payee Portal.
+    ''')
+
+
+if __name__ == '__main__':
+    main()
