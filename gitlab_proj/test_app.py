ec1c87b69c7d3e8a7b960ef1e3c67900a3781f9e
iroshanav <knightshift.kurosaki@gmail.com>
2024-08-18 11:57:22 +0530

    Update | Add comments for better clarification

--- gitlab_proj/test_app.py
+++ gitlab_proj/test_app.py
@@ -1,12 +1,16 @@
+# test file should be named as test_*.py
+# or *_test.py
 from app import app
 
 
+# need to start with test_{function name}
 def test_index():
     client = app.test_client()
     response = client.get('/')
     assert response.status.code == 200
 
 
+# response checking should be test_{function name}_response
 def test_index_response():
     client = app.test_client()
     response = client.get('/')
