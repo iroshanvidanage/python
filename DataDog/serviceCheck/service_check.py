8276af40e11204a72df5bc6a3621f8d5ce7c34ae
iroshanav <knightshift.kurosaki@gmail.com>
2025-02-23 11:10:29 +0530

    Update | service check dd integration

--- DataDog/serviceCheck/service_check.py
+++ DataDog/serviceCheck/service_check.py
@@ -6,13 +6,13 @@ from checks import AgentCheck
 import os
 import subprocess
 
-class tlsCheck(AgentCheck):
+class serviceCheck(AgentCheck):
     def check(self, instance):
 
         service_name = 'nginx'
-        service_avail_command = f'systemctl list-units --full -all | grep -Fq "{service_name}.service"'
+        service_avail_command = 'systemctl list-units --full -all | grep -Fq "nginx.service"'
         service_status_command = f'systemctl status {service_name}'
-        instance_id = None
+        service_active_command = service_status_command + " | awk 'NR==3{print$1$2}'"
         datadog_gauge = f'service_availability.{service_name}'
         status = {
             'active' : 1,
@@ -21,17 +21,15 @@ class tlsCheck(AgentCheck):
         }
         
         try:
-            service_available = subprocess.run(service_avail_command, capture_output=True, shell=True, text=True)
+            service_available = subprocess.check_output(service_avail_command, shell=True, text=True).decode('utf-8')
             if not service_available:
                 warning_string = f"The service {service_name} is not installed in the instance."
                 self.log.warning(warning_string)
                 return
             
-            service_status = subprocess.check_output(service_avail_command, shell=True, text=True)
-
-            if service_status:
-                ...
-
-            self.gauge()
+            service_status = subprocess.check_output(service_active_command, shell=True, text=True).decode('utf-8')
+            # service_status = 'Active:active'
+            self.log.info(service_status)
+            self.gauge(datadog_gauge, status[service_status.split(':')[-1]])
         except Exception as e:
             self.log.error("Error: %s", str(e))
